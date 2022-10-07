02739579152b0b79724baf86d3ac64fed753ac26
ErickyAll <erick227.ea21@gmail.com>
2022-10-07 13:58:25 -0300

    add form component

---
+++ src/components/pages/NewProject/NewProject.js
@@ -0,0 +1,12 @@
+import ProjectForm from '../../project/ProjectForm/ProjectForm'
+import './style.css'
+
+export default function NewProject() {
+  return (
+    <div className="new_container">
+      <h1>New Project</h1>
+      <p>Crie seu projeto e depois adicione os serviços</p>
+      <ProjectForm />
+    </div>
+  )
+}
