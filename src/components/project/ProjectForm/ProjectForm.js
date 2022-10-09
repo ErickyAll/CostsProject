dba2394ae774100679fcc1bead2ade4d50420cf2
ErickyAll <erick227.ea21@gmail.com>
2022-10-09 18:28:17 -0300

    add api fake

--- src/components/project/ProjectForm/ProjectForm.js
+++ src/components/project/ProjectForm/ProjectForm.js
@@ -1,9 +1,27 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../../form/Input/Input'
 import Select from '../../form/select/Select'
 import Submit from '../../form/submit/Submit'
 import './style.css'
 
 export default function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        setCategories(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <form className="form">
       <Input
@@ -19,7 +37,11 @@ export default function ProjectForm({ btnText }) {
         placeholder="Insira o orçamento do projeto"
       />
 
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
 
       <Submit text={btnText} />
     </form>
