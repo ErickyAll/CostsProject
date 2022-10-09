09c5c3b95b6cd7721812806fedbf709948149916
ErickyAll <erick227.ea21@gmail.com>
2022-10-08 22:11:08 -0300

    new branch

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './components/Footer/Footer'
 
 function App() {
   return (
+    // updates futuros virão nesta branch
     <Router>
       <Navbar />
       <Container customClass="min-height">
