cb53b38b0b74e3558150987e51572c54f0d50784
ErickyAll <erick227.ea21@gmail.com>
2022-11-19 11:18:19 -0300

    editPrioject

--- src/components/pages/Project/Project.js
+++ src/components/pages/Project/Project.js
@@ -1,28 +1,65 @@
 import './style.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../../layout/Loading/Loading'
+import Container from '../../layout/Container/Container'
 
 export default function Project() {
   const { id } = useParams()
   const [project, setProject] = useState([])
+  const [showPForm, setShowPForm] = useState(false)
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(resp => resp.json())
-      .then(data => {
-        setProject(data)
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
-      .catch(err => console.log(err))
+        .then(resp => resp.json())
+        .then(data => {
+          setProject(data)
+        })
+        .catch(err => console.log(err))
+    }, 5000)
   }, [id])
+
+  function toggleprojectForm() {
+    setShowPForm(!showPForm)
+  }
 
   return (
     <>
-      <p>Project</p>
+      {project.name ? (
+        <div>
+          <Container customClass="column">
+            <div>
+              <h1>Projeto: {project.name}</h1>
+              <button onClick={toggleprojectForm}>
+                {!showPForm ? 'Edit Project' : 'Close'}
+              </button>
+              {!showPForm ? (
+                <div>
+                  <p>
+                    <span>Category: </span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total Orçamento:</span>R${project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado:</span>R${project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div>details</div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </>
   )
 }
